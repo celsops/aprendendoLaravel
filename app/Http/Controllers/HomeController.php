1335355f667056d1da012d4fcff35e90144f6c65
Celso <celsovasconcelos.27@gmail.com>
2019-07-03 09:29:01 -0300

    Auto stash before merge of "master" and "origin/master"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,10 +25,37 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index(Request $request)
+<<<<<<< Updated upstream
     {   
         $email = Auth::user()->email;
         $notes = Note::where('email_fk',$email) -> get();
         //$notes = DB::select('select * from notes where email_fk = :email', ['email' => Auth::user()->email]);
+=======
+    {
+        // var_dump($notes);
+        $param = $request -> all();
+        
+        // var_dump($param);
+
+        if(isset($param['id'])){
+            $id = $param['id'];
+        }   
+        if(isset($param['edit'])){
+            // echo "edit";
+            $note = Note::find($id);
+            // var_dump($note);
+            return view('update',compact('note'));
+            // return redirect('/update',compact('note'));
+        }
+        else if(isset($param['delete'])){
+            echo "delete";
+            Note::where('id',$id) -> delete();
+        }
+        // $notes = Note::all();
+        $email = Auth::user() -> email;
+        $notes = Note::where('email_fk',$email) -> get();
+
+>>>>>>> Stashed changes
 
         return view('home', compact('notes'));
         
